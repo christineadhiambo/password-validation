e6712b457619c27ac29f2ad070af5e913a222929
Christine Adhiambo <151001291+christineadhiambo@users.noreply.github.com>
2025-05-09 13:43:35 +0000

    Update from Codespaces

--- Christine_Adhiambo.js
+++ Christine_Adhiambo.js
@@ -1,1 +1,16 @@
+function password(str) {
+     // Check minimum length
+     if (str.length < 8) return false;
 
+     // Check for at least 1 uppercase letter
+     if (!/[A-Z]/.test(str)) return false;
+
+     // Check for at least 1 lowercase letter
+     if (!/[a-z]/.test(str)) return false;
+
+     // Check for at least 1 number
+     if (!/[0-9]/.test(str)) return false;
+
+     // If all conditions are met, return true
+     return true;
+   }
